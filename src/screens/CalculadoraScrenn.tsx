c1bca671d738e6679c244bd68f568b0b05f0b518
Javier-ojeda24 <richardjavierojeda2021@gmail.com>
2023-07-28 11:11:02 -0300

    agregando los numeros

--- src/screens/CalculadoraScrenn.tsx
+++ src/screens/CalculadoraScrenn.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {Text, View} from 'react-native';
-import { styles } from '../theme/appTheme';
+import {styles} from '../theme/appTheme';
 
 export const CalculadoraScrenn = () => {
-  return(
-     <Text style={styles.texto}>CalculadoraScrenn</Text>
+  return (
+    <View style={styles.calculadoraContainer}>
+      <Text style={styles.resultadoPequeño}>1,500.00</Text>
+      <Text style={styles.resultado}>1,500.00</Text>
+      <View>
+        {/* Boton */}
+        <View style={styles.boton}>
+          <Text style={styles.botonTexto}>1</Text>
+        </View>
+      </View>
+    </View>
   );
 };
--- src/theme/appTheme.tsx
+++ src/theme/appTheme.tsx
@@ -5,8 +5,34 @@ export const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: 'black',
   },
-  texto: {
+  calculadoraContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'flex-end',
+  },
+  resultadoPequeño: {
+    color: 'rgba(255,255,255,0.5)',
+    fontSize: 30,
+    textAlign: 'right',
+  },
+  resultado: {
     color: 'white',
-    fontSize: 20,
+    fontSize: 60,
+    textAlign: 'right',
+  },
+  boton: {
+    height: 80,
+    width: 80,
+    color: 'white',
+    backgroundColor: '#9b9b9b',
+    borderRadius: 100,
+    justifyContent: 'center',
+  },
+  botonTexto: {
+    textAlign: 'center',
+    padding: 10,
+    fontSize: 30,
+    color: 'black',
+    fontWeight: 'bold',
   },
 });
